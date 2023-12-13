748c0db3f1a6b53673f7b283f0336df4c3709be5
ReynaRodriguess <93771498+ReynaRodriguess@users.noreply.github.com>
2023-12-13 15:37:20 +0000

    Delete TPPoo directory

--- TPPoo/src/Professores.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-public class Professores extends funcionario implements Serializable {
-    public Professores (int id, int idade, String nome){
-        this.setId(id);
-        this.setIdade(idade);
-        this.setNome(nome);
-    }
-}
